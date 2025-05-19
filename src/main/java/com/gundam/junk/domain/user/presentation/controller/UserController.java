0bd68e5db3b750c12f15665bee5252b71b1ccb15
Imnotcoderdude <topsolo7@naver.com>
2025-05-19 16:43:44 +0900

    feat[#1]: 사용자 Controller 클래스 추가

---
+++ src/main/java/com/gundam/junk/domain/user/presentation/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.gundam.junk.domain.user.presentation.controller;
+
+import com.gundam.junk.domain.user.application.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+}
