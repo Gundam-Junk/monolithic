9664da3ffb0c125fbfd95fd1ac3dec3a2aea6184
Imnotcoderdude <topsolo7@naver.com>
2025-05-21 23:50:13 +0900

    feat[#1]: 인증 서버로부터 요청받을 내부용 회원가입 컨트롤러 추가

--- src/main/java/com/gundam/junk/domain/user/presentation/internal/controller/UserInternalController.java
+++ src/main/java/com/gundam/junk/domain/user/presentation/internal/controller/UserInternalController.java
@@ -1,9 +1,16 @@
 package com.gundam.junk.domain.user.presentation.internal.controller;
 
+import com.gundam.junk.domain.user.application.dto.interanl.request.InternalUserSignupRequestDto;
+import com.gundam.junk.domain.user.application.dto.interanl.response.InternalUserSignupResponseDto;
 import com.gundam.junk.domain.user.application.service.UserService;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,4 +19,17 @@ public class UserInternalController {
 
     private final UserService userService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<InternalUserSignupResponseDto> signup(
+        @RequestBody InternalUserSignupRequestDto requestDto
+    ) {
+        InternalUserSignupResponseDto responseDto = userService.signup(requestDto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/api/v1/users/my-page")
+            .build()
+            .toUri();
+
+        return ResponseEntity.created(uri).body(responseDto);
+    }
 }
