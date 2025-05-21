08d1d0e671bd1b6b86ccdb8fc202e339703eb86d
Imnotcoderdude <topsolo7@naver.com>
2025-05-21 23:49:52 +0900

    feat[#1]: 인증 서버를 통해 오는 회원가입 요청에 대해 사용자를 저장하고 반환할 서비스 메서드 작성

--- src/main/java/com/gundam/junk/domain/user/application/service/UserService.java
+++ src/main/java/com/gundam/junk/domain/user/application/service/UserService.java
@@ -1,5 +1,9 @@
 package com.gundam.junk.domain.user.application.service;
 
+import com.gundam.junk.domain.user.application.dto.interanl.request.InternalUserSignupRequestDto;
+import com.gundam.junk.domain.user.application.dto.interanl.response.InternalUserSignupResponseDto;
+
 public interface UserService {
 
+    InternalUserSignupResponseDto signup(InternalUserSignupRequestDto requestDto);
 }
--- src/main/java/com/gundam/junk/domain/user/application/service/impl/UserServiceImpl.java
+++ src/main/java/com/gundam/junk/domain/user/application/service/impl/UserServiceImpl.java
@@ -1,7 +1,12 @@
 package com.gundam.junk.domain.user.application.service.impl;
 
+import com.gundam.junk.domain.user.application.dto.interanl.request.InternalUserSignupRequestDto;
+import com.gundam.junk.domain.user.application.dto.interanl.response.InternalUserSignupResponseDto;
+import com.gundam.junk.domain.user.application.mapper.UserMapper;
 import com.gundam.junk.domain.user.application.service.UserService;
+import com.gundam.junk.domain.user.domain.model.User;
 import com.gundam.junk.domain.user.domain.repository.UserRepository;
+import com.gundam.junk.domain.user.domain.valodator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +15,15 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserValidator userValidator;
 
+    @Override
+    public InternalUserSignupResponseDto signup(InternalUserSignupRequestDto requestDto) {
+        User user = UserMapper.internalSignupToEntityReq(requestDto);
+        userValidator.internalSignupValidate(user);
+
+        userRepository.save(user);
+        return UserMapper.internalEntityToSignupRes(user);
+    }
 
 }
