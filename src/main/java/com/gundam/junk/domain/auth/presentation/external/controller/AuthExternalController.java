bf9ff5c1017ea5929c841c8e3fd57c90806b3f50
Imnotcoderdude <topsolo7@naver.com>
2025-06-13 18:33:27 +0900

    feat[#1]: 컨트롤러 회원가입 API 작성

--- src/main/java/com/gundam/junk/domain/auth/presentation/external/controller/AuthExternalController.java
+++ src/main/java/com/gundam/junk/domain/auth/presentation/external/controller/AuthExternalController.java
@@ -1,9 +1,16 @@
 package com.gundam.junk.domain.auth.presentation.external.controller;
 
+import com.gundam.junk.domain.auth.application.dto.external.request.ExternalAuthSignupRequestDto;
+import com.gundam.junk.domain.auth.application.dto.external.response.ExternalAuthSignupResponseDto;
 import com.gundam.junk.domain.auth.application.service.AuthService;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,4 +19,16 @@ public class AuthExternalController {
 
     private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<ExternalAuthSignupResponseDto> signup(
+        @RequestBody ExternalAuthSignupRequestDto requestDto
+    ) {
+        ExternalAuthSignupResponseDto responseDto = authService.signup(requestDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/api/v1/users/my-page")
+            .build()
+            .toUri();
+
+        return ResponseEntity.created(uri).body(responseDto);
+    }
 }
