e903fbdcc0208b043afee726d50ac9bfcf73b207
Imnotcoderdude <topsolo7@naver.com>
2025-06-16 10:17:30 +0900

    feat[#1]: Auth 서비스 매퍼 클래스 메서드 추가

--- src/main/java/com/gundam/junk/domain/auth/application/mapper/AuthMapper.java
+++ src/main/java/com/gundam/junk/domain/auth/application/mapper/AuthMapper.java
@@ -1,5 +1,16 @@
 package com.gundam.junk.domain.auth.application.mapper;
 
+import com.gundam.junk.domain.auth.application.dto.external.request.ExternalAuthSignupRequestDto;
+import com.gundam.junk.domain.auth.application.dto.external.response.ExternalAuthSignupResponseDto;
+
 public class AuthMapper {
 
+    public static ExternalAuthSignupResponseDto toSignupRes(ExternalAuthSignupRequestDto requestDto,
+        String password) {
+        return ExternalAuthSignupResponseDto.of(
+            requestDto.getLoginId(),
+            requestDto.getUsername(),
+            "USER"
+        );
+    }
 }
